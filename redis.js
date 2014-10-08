3d238f6fdfa46e422cbb04ab6847027f66a8cbd5
constantology <constantology@gmail.com>
2014-10-08 14:26:36 +0100

    added debug code

--- redis.js
+++ redis.js
@@ -2,7 +2,9 @@ var getenv = require('getenv');
 var redis = require('redis');
 
 var REDIS_PORT = getenv('REDIS_PORT_6379_TCP_PORT', '6379');
-var REDIS_HOST = getenv('REDIS_PORT_6379_TCP_ADDR', 'localhost');
+var REDIS_HOST = getenv('REDIS_PORT_6379_TCP_ADDR', '127.0.0.1');
+
+console.log('REDIS CONNECTION: ' + REDIS_HOST + ':' + REDIS_PORT);
 
 var redisClient = redis.createClient(REDIS_PORT, REDIS_HOST);
 var redisCo = require('co-redis')(redisClient);
