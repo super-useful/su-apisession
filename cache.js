56220439b1b4e1ae7e99443cb8c0ec65bd524414
constantology <constantology@gmail.com>
2014-11-03 12:25:47 +0000

    allow turning memcache on/off from config

--- cache.js
+++ cache.js
@@ -1,28 +1,41 @@
+var CONF = require('config');
 var session = require('./');
 
 module.exports = {
   get : function* getCached(next) {
-    try {
-      var data = yield session.getCached(this.su.req.xcsrf, this.request.method + ':' + this.request.url);
+    if (CONF.app.session.memcache !== false) {
+      var key = this.request.method + ':' + this.request.url;
 
-      if (data) {
-        this.su.req.cached = data;
+      try {
+        var data = yield session.getCached(this.su.req.xcsrf, key);
+
+        if (data) {
+          this.su.req.cached = data;
+
+          process.emit('app:log', module, 'MEMCACHE:GET\t' + key);
+        }
       }
-    }
-    catch (e) {
-      process.emit('app:debug', module, JSON.stringify(e.stack));
+      catch (e) {
+        process.emit('app:debug', module, JSON.stringify(e.stack));
+      }
     }
 
     yield next;
   },
   set : function* cacheResponse(next) {
-    try {
-      if (!this.su.req.cached && this.status === 200) {
-        yield session.cache(this.su.req.xcsrf, this.request.method + ':' + this.request.url, this.data);
+    if (CONF.app.session.memcache !== false) {
+      var key = this.request.method + ':' + this.request.url;
+
+      try {
+        if (!this.su.req.cached && this.status === 200) {
+          yield session.cache(this.su.req.xcsrf, key, this.data);
+
+          process.emit('app:log', module, 'MEMCACHE:SET\t' + key);
+        }
       }
-    }
-    catch (e) {
-      process.emit('app:debug', module, JSON.stringify(e.stack));
+      catch (e) {
+        process.emit('app:debug', module, JSON.stringify(e.stack));
+      }
     }
 
     yield next;
